155f7a05be3182e1efcfe88caacbba192643cf7d
MattMorgis <matt@rappiddevelopment.com>
2018-10-12 18:21:10 -0400

    fix: bug that would leave out some variations

--- count-takes.js
+++ count-takes.js
@@ -2,12 +2,24 @@ const fs = require('fs');
 const JSONStream = require('JSONStream');
 const es = require('event-stream');
 
-const episode = '0002-Art-Of-The-Take'
+const episode = '0001-Art-Of-The-Take'
+
+const occurrences = (mainString, subString) => {
+  mainString += '';
+  subString += '';
+
+  if (subString.length <= 0) {
+    return mainString.length + 1;
+  }
+
+  subStr = subString.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  return (mainString.match(new RegExp(subStr, 'gi')) || []).length;
+}
 
 fs.createReadStream(__dirname + '/transcripts/' + episode + '.json')
   .pipe(JSONStream.parse('results.transcripts.*'))
   .pipe(es.mapSync((data) => {
-    const takeCount = (data.transcript.match(/take/g) || []).length;
+    const takeCount = occurrences(data.transcript, 'take', true);
     console.log(episode);
     console.log('Take count: ');
     console.log(takeCount)

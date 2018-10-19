4e0b322eb56cb4348a41c93bc4d1f16585ac495c
MattMorgis <matt@rappiddevelopment.com>
2018-10-19 16:31:53 -0400

    feat: adding ep 3 text stream

--- generate-txt-stream.js
+++ generate-txt-stream.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const JSONStream = require('JSONStream');
 const es = require('event-stream');
 
-fs.createReadStream(__dirname + '/transcripts/0002-Art-Of-The-Take.json')
+fs.createReadStream(__dirname + '/transcripts/0003-Art-Of-The-Take.json')
   .pipe(JSONStream.parse('results.transcripts.*'))
   .pipe(es.mapSync((data) => {
     return data.transcript
   }))
-  .pipe(fs.createWriteStream(__dirname + '/transcripts/0002-Art-Of-The-Take.txt'))
+  .pipe(fs.createWriteStream(__dirname + '/transcripts/0003-Art-Of-The-Take.txt'))

ada4b4feb84231f730b242f65499ea6a83b32590
Matt Morgis (ELS-PHI) <m.morgis@elsevier.com>
2018-10-26 12:57:10 -0400

    refactor: make episode a const

--- generate-txt-stream.js
+++ generate-txt-stream.js
@@ -1,10 +1,14 @@
-const fs = require('fs');
-const JSONStream = require('JSONStream');
-const es = require('event-stream');
+const fs = require("fs");
+const JSONStream = require("JSONStream");
+const es = require("event-stream");
 
-fs.createReadStream(__dirname + '/transcripts/0003-Art-Of-The-Take.json')
-  .pipe(JSONStream.parse('results.transcripts.*'))
-  .pipe(es.mapSync((data) => {
-    return data.transcript
-  }))
-  .pipe(fs.createWriteStream(__dirname + '/transcripts/0003-Art-Of-The-Take.txt'))
+const episode = "0004-Art-Of-The-Take";
+
+fs.createReadStream(__dirname + "/transcripts/" + episode + ".json")
+  .pipe(JSONStream.parse("results.transcripts.*"))
+  .pipe(
+    es.mapSync(data => {
+      return data.transcript;
+    })
+  )
+  .pipe(fs.createWriteStream(__dirname + "/transcripts/" + episode + ".txt"));

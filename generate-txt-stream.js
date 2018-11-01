fd9ec7c4e53bff47a06153314f4aa975cbe4f94e
MattMorgis <matt@rappiddevelopment.com>
2018-11-01 14:54:25 -0400

    Revert "refactor: make const for episode"
    This reverts commit bda354b5082182b045a49b54a1b6ccb2f2314856.

--- generate-txt-stream.js
+++ generate-txt-stream.js
@@ -2,11 +2,9 @@ const fs = require('fs');
 const JSONStream = require('JSONStream');
 const es = require('event-stream');
 
-const episode = '0005-Art-Of-The-Take'
-
-fs.createReadStream(__dirname + '/transcripts/' + episode + '.json')
+fs.createReadStream(__dirname + '/transcripts/0003-Art-Of-The-Take.json')
   .pipe(JSONStream.parse('results.transcripts.*'))
   .pipe(es.mapSync((data) => {
     return data.transcript
   }))
-  .pipe(fs.createWriteStream(__dirname + '/transcripts/' + episode + '.txt '))
+  .pipe(fs.createWriteStream(__dirname + '/transcripts/0003-Art-Of-The-Take.txt'))
